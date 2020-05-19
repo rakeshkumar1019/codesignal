3fadabc9792804ef272c703569e6569b8b477723
Rakesh Kumar  Singh <52422856+rakeshkumar1019@users.noreply.github.com>
2020-05-19 20:07:09 +0530

    Update firstDuplicate.cpp

--- firstDuplicate.cpp
+++ firstDuplicate.cpp
@@ -1,5 +1,9 @@
-int firstDuplicate(std::vector<int> a) {
-     map<int,int> m;
+#include<bits/stdc++.h>
+#include<map>
+#include<iostream>
+using namespace std;
+int firstDuplicate(vector<int> a){
+  map<int,int> m;
  int n=a.size();
  for(int i=0;i<n;i++){
    if(m[a[i]]==1){
@@ -8,4 +12,18 @@ int firstDuplicate(std::vector<int> a) {
      m[a[i]]=1;
  }
  return -1;
+}
+int main(){
+  int n;
+  cin>>n;
+  vector<int> a;
+  for(int i=0;i<n;i++){
+    int temp;
+    cin>>temp;
+    a.push_back(temp);
+  }
+  cout<<firstDuplicate(a)<<endl;
+  
+
+
 }
